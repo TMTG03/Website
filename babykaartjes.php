0d785f947b2e66e124aa1aeaf59d36e206949679
didekoning <didekoning@gmail.com>
2013-11-14 11:37:44 +0100

    pagina

---
+++ babykaartjes.php
@@ -0,0 +1,77 @@
+<? require_once('connection.php') ?>
+<!doctype html>
+<html manifest="thema.appcache">
+<head>
+<meta charset="utf-8">
+<title>.: Info :.</title>
+<link rel="icon" href="img/favicon.ico" type="image/x-icon"/>
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
+<link rel="stylesheet" type="text/css" href="css/style.css" title="default">
+<link rel="roze stylesheet" type="text/css" href="css/style_roze.css" title="roze" />
+<link rel="blauwroze stylesheet" type="text/css" href="css/style_blauw_roze.css" title="blauwroze" />
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="scripts/switcher.js"></script>
+</head>
+
+<body>
+<div id="container">
+  <div id="headercolor">
+    <div id="container_breedte">
+      <header id="logo_plek"><a href="index.php" id="logo"><img width="333" src="img/logo.png" alt="" /></a></header>
+      <nav id="menu">
+        <ul>
+          <li class='active'><a href='index.php'><span>Home</span></a></li>
+          <li class='has-sub'><a href='#'><span>Babykaartjes</span></a>
+            <ul>
+              <li class='has-sub'><a href='#'><span>tekst</span></a>
+                <ul>
+                  <li><a href='#'><span>tekst</span></a></li>
+                  <li class='last'><a href='#'><span>tekst</span></a></li>
+                </ul>
+              </li>
+              <li class='has-sub'><a href='#'><span>tekst</span></a>
+                <ul>
+                  <li><a href='#'><span>tekst</span></a></li>
+                  <li class='last'><a href='#'><span>tekst</span></a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href='info.php'><span>Informatie</span></a></li>
+          <li><a href='login.php'><span>Inloggen</span></a></li>
+          <li class='last'><a href='contact.php'><span>Contact</span></a></li>
+        </ul>
+        <div id="styleswitchen">
+          <div id="styleswitchvak1"><a href="#" onclick="setActiveStyleSheet('default'); return false;"><img src="img/clear.png" height="20" width="20" alt="" /></a></div>
+          <div id="styleswitchvak2"><a href="#" onclick="setActiveStyleSheet('roze'); return false;"><img src="img/clear.png" height="20" width="20" alt="" /></a></div>
+          <div id="styleswitchvak3"><a href="#" onclick="setActiveStyleSheet('blauwroze'); return false;"><img src="img/clear.png" height="20" width="20" alt="" /></a></div>
+        </div>
+      </nav>
+    </div>
+  </div>
+  <div class="blauwelijn"></div>
+  <div id="tussen_balk"></div>
+  <div id="titelbalk">El babykaartjes</div>
+  <hr class="schaduw_lijn"></hr>
+  <div id="container_content">
+  <br/>
+  <br/>
+  <? require_once('lorum.txt') ?>
+  </div>
+  <footer id="footer">
+    <div class="blauwelijn"></div>
+    <div id="footer_content">
+      <div id="footer_socialmedia_iconen">
+        <a href="https://plusone.google.com/_/+1/confirm?hl=en&amp;url=http://tmtg03.ict-lab.nl/website" target="_blank"><img src="img/google.png" alt="" /></a>&nbsp;
+        <a href="https://www.facebook.com/sharer/sharer.php?u=http://tmtg03.ict-lab.nl/website" target="_blank"><img src="img/facebook.png" alt="" /></a>&nbsp;
+        <a href="http://twitter.com/home?status=http://tmtg03.ict-lab.nl/website" target="_blank"><img src="img/twitter.png" alt="" /></a>
+      </div>
+      <div id="footer_copyright">
+        <p class="copyright_tekst">&copy; 2013 www.babyberichten.nl</p>
+      </div>
+    </div>
+  </footer>
+</div>
+</body>
+</html>
