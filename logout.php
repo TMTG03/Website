49d69a1d416abf7c9454ad6fe29f7b1d58cd3e0d
Owain van Brakel <owain.vanbrakel@gmail.com>
2013-11-19 21:49:29 +0100

    uitleg waarom we php hier niet afsluiten

--- logout.php
+++ logout.php
@@ -5,3 +5,7 @@
 	// doorlinken naar login met een variable om te filteren dat er uitgelogd is
     header("Location: login.php?logout=true"); 
     die("doorlinken naar login.php?logout=true");
+	// we willen natuurlijk geen trailing white spaces zien...
+	// vandaar dat we php niet afsluiten hier
+	//
+	// http://php.net/manual/en/language.basic-syntax.instruction-separation.php
