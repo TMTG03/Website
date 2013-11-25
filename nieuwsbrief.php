9e51157a3e3f40be29c728f5e4a21b3a01d3abc6
didekoning <didekoning@gmail.com>
2013-11-25 12:46:40 +0100

    begin aanpassen pdo

--- nieuwsbrief.php
+++ nieuwsbrief.php
@@ -1,3 +1,49 @@
+<?
+	require_once("connection.php");
+			$query = " 
+				SELECT 
+					* 
+				FROM users 
+				WHERE  
+			";	
+			try { 
+				$stmt = $db->prepare($query); 
+				$result = $stmt->execute; 
+			} catch(PDOException $ex) {
+				die("FOUT: " . $ex->getMessage()); 
+				$PDOException = true;
+			} 
+
+
+
+
+
+
+
+//zuidholland
+			$query = " 
+				SELECT 
+					* 
+				FROM babykaartjes 
+				WHERE 
+					provincie = Zuid-Holland 
+			";			
+			 
+			try { 
+				$stmt = $db->prepare($query); 
+				$result = $stmt->execute; 
+			} catch(PDOException $ex) {
+				die("FOUT: " . $ex->getMessage()); 
+				$PDOException = true;
+			} 
+		
+
+  
+	
+   
+
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -8,29 +54,6 @@
 
 <body>
 
-<?
-	require_once("connection.php");
-	$opdracht = "SELECT * FROM users";
-	$result = mysql_query($opdracht);
-	while ($Rij = mysql_fetch_array($result)){
-		if ($provincie == "Groningen")
-		{
-			$opdracht2 = "SELECT * FROM babyberichten WHERE $provincie = Groningen";
-		}
-		if ($provincie == "Zuid-Holland")
-		{
-			$opdracht2 = "SELECT * FROM babyberichten WHERE $provincie = Zuid-Holland";
-		}
-	}
 
-	
-
-	
-  
-	
-   
-
-
-?>
 </body>
 </html>
